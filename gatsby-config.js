5311d525bbdd454c5ead7784925dbd7ae88370b6
JobMusembi <jobmusembim@gmail.com>
2024-04-12 14:47:17 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `thetwelve`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
