3975f052f73458fa2d1d33cc41953117836296ee
JobMusembi <jobmusembim@gmail.com>
2024-09-27 18:30:59 +0300

    vf

--- src/components/passportphotos.js
+++ src/components/passportphotos.js
@@ -1,7 +1,7 @@
 // src/components/PassportPhotos.js
 import React from 'react';
 import image1 from '../images/his.jpg';
-import image2 from '../images/hers.jpg';
+//import image2 from '../images/hers.jpg';
 //import './PassportPhotos.css';
 
 const PassportPhotos = () => (
@@ -10,12 +10,12 @@ const PassportPhotos = () => (
     <div className="image-container">
     <div className="image-caption">
       <img src={image1} alt="job" />
-      <div className="caption">Oleg - DESIGNER / DEVELOPER</div>
+      <div className="caption">M.M - DESIGNER / DEVELOPER</div>
       </div>
-      <div className="image-caption">
+      {/* <div className="image-caption">
         <img src={image2} alt="faith" />
         <div className="caption">Olga - DEVELOPER / UX</div>
-      </div>
+      </div> */}
     </div>
   </div>
 );
